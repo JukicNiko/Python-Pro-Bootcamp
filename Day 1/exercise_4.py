824147771ee213e896f4e2928082c2679a5dc143
Niko Jukić <herolander200@gmail.com>
2022-12-21 00:57:46 +0100

    Working with Variables in Python to Manage Data

---
+++ Day 1/exercise_4.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+a2 = b
+b2 = a
+
+a = a2 
+b = b2
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
---
+++ Day 1/exercise_5.py
@@ -0,0 +1,4 @@
+cit = input("What is the name of your city?\n")
+pet = input("What is the name of your pet?\n")
+
+print("The name of your band is: " + city + " " + pet)
